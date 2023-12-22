8759d849de4ca4162c92c5be632b90d1c6524d92
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2023-12-22 21:30:25 +0300

    Complete lab 5.

---
+++ Lab5/Shaders/frag.shader.glsl
@@ -0,0 +1,40 @@
+﻿#version 330
+
+// Устанавливаем точность для вычислений с плавающей запятой
+precision mediump float;
+
+
+uniform sampler2D uSampler;
+
+
+in vec2 vTexCoord;      // Координаты текстуры
+in vec3 vNormal;        // Нормаль объекта
+in vec3 vViewDirection; // Вектор обзора
+
+
+out vec4 fragColor;
+
+void main()
+{
+    // Интенсивности освещения
+    vec3 ambientLightIntensity = vec3(0.7, 0.7, 0.7);
+    vec3 sunLightIntensity = vec3(0.7, 0.7, 0.7);
+    
+    // Направление света от солнца
+    vec3 sunLightDirection = normalize(vec3(-20.0, 20.0, 20.0));
+
+    // Корректируем координаты текстуры для вертикального отражения
+    vec2 flipped = vec2(vTexCoord.x, 1 - vTexCoord.y);
+
+    // Получаем цвет пикселя из текстуры
+    vec4 texel = texture(uSampler, flipped);
+
+    // Рассчитываем toon-shading 
+    float tunShading = max(dot(vNormal, vViewDirection), 0.0);
+
+    // Рассчитываем освещение с учетом toon-shading
+    vec3 lightIntensity = ambientLightIntensity + sunLightIntensity * max(dot(vNormal, sunLightDirection), 0.0) * tunShading;
+
+    // Устанавливаем итоговый цвет фрагмента с учетом освещенности и текстуры
+    fragColor = vec4(texel.rgb * lightIntensity, texel.a);
+}
---
+++ Lab5/Shaders/vert.shader.glsl
@@ -0,0 +1,18 @@
+﻿#version 330
+
+layout(location=0) in vec3 aPosition;
+layout(location=1) in vec2 aTexCoord;
+layout(location = 2) in vec3 aNormal;
+
+uniform mat4 uMvpMatrix;
+uniform mat4 uModelMatrix;
+
+out vec2 vTexCoord;
+out vec3 vNormal;
+
+void main()
+{
+    gl_Position = uMvpMatrix * vec4(aPosition, 1.0);
+    vNormal = (uModelMatrix * vec4(aNormal, 0.0)).xyz;
+    vTexCoord = aTexCoord;
+}
